7d006b9824e76cb434ff8d5beff7adf5d4f2d486
Andreo Barros <andreo.barros@2ndmarket.com.br>
2021-10-05 07:08:37 -0300

    Docker stats saving function output correction

--- python_consumer_producer_pair/custom_compose/auxiliary_functions/save_n_read_data.py
+++ python_consumer_producer_pair/custom_compose/auxiliary_functions/save_n_read_data.py
@@ -8,6 +8,8 @@ def save_docker_stats(iteration_code, stats_stdout, current_dir):
     save_stats_graph(current_dir, f'stats_kafka_{iteration_code}.txt', f'stats_kafka_{iteration_code}.png', False)
     save_stats_graph(current_dir, f'stats_kafka_{iteration_code}.txt', f'stats_kafka_{iteration_code}_free_scales.png', True)
 
+    return [f'stats_kafka_{iteration_code}.txt', f'stats_kafka_{iteration_code}.png', f'stats_kafka_{iteration_code}_free_scales.png']
+
 def container_stats_target(machine, container):
     import subprocess
     container = subprocess.Popen(['docker', '-H', machine, 'ps', '-q', '-f', f'name={container}'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
