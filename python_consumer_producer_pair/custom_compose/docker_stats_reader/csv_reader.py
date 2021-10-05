7650ff5883a5b39c03e18d39cee05e229adc6918
Andreo Barros <andreo.barros@2ndmarket.com.br>
2021-10-05 06:59:04 -0300

    Correction at csv_reader memory readings

--- python_consumer_producer_pair/custom_compose/docker_stats_reader/csv_reader.py
+++ python_consumer_producer_pair/custom_compose/docker_stats_reader/csv_reader.py
@@ -11,7 +11,7 @@ def save_stats_graph(file_path, file_to_open, file_to_save, loose_scales):
     mem_tmp2 = pd.DataFrame(panda_csv['mem_usage / limit'].str.split('/',1).tolist(),
                                     columns = ['mem_usage','limit'], index = panda_csv.index)
 
-    mem_usag = mem_tmp2['mem_usage'].str.replace('MiB', '')
+    mem_tmp2 = mem_tmp2['mem_usage'].str.replace('MiB', '')
     mem_usag = mem_tmp2['mem_usage'].str.replace('GiB', '')
     mem_usag = pd.to_numeric(mem_usag, downcast='float')
 
