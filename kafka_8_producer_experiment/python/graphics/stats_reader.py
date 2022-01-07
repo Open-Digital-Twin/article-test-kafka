3625e46956ea94e32fbe7f1e7c8a97369643c8ad
Andreo Barros <andreo.barros@2ndmarket.com.br>
2022-01-06 22:17:59 -0300

    New version of kafka-stats reader

--- kafka_8_producer_experiment/python/graphics/stats_reader.py
+++ kafka_8_producer_experiment/python/graphics/stats_reader.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from os import makedirs
 
 from auxiliaryfunctions.terminal import print_centralized
@@ -11,47 +12,88 @@ def create_stats_graph(exp_num= '', file_to_open= '', loose_scales= True, save_i
 
     panda_csv = pd.read_csv(file_path + 'csv/' + file_to_open, usecols=[1,2], names=['cpu_%', 'mem_usage / limit'])
 
+    panda_csv.replace('', np.nan, inplace=True)
+    panda_csv = panda_csv.dropna()
+
+
     x = panda_csv.index
     cpu_perc = panda_csv['cpu_%'].str.replace('%', '')
     cpu_perc = pd.to_numeric(cpu_perc, downcast='float')
 
-    mem_tmp2 = pd.DataFrame(panda_csv['mem_usage / limit'].str.split('/',1).tolist(),
-                                        columns = ['mem_usage','limit'], index = panda_csv.index)
+    net_io_tmp = pd.DataFrame(panda_csv['NetI/O'].str.split('/', 1).tolist(),
+                                        columns = ['Net In', 'Net Out'], index = panda_csv.index)
+
+    mem_tmp2 = pd.DataFrame(panda_csv['mem_usage / limit'].str.split('/', 1).tolist(),
+                                        columns = ['mem_usage', 'limit'], index = panda_csv.index)
 
 
+    net_in = net_io_tmp['Net In'].str.replace('kB', '')
+    net_in = net_in.str.replace('MB', '')
+    net_in = net_in.str.replace('"', '')
+    net_in = pd.to_numeric(net_in, downcast='float')
+
+    net_out = net_io_tmp['Net Out'].str.replace('kB', '')
+    net_out = net_out.str.replace('MB', '')
+    net_out = net_out.str.replace('"', '')
+    net_out = pd.to_numeric(net_out, downcast='float')
+
+    mem_usag = mem_tmp2['mem_usage'].str.replace('MiB', '')
     mem_tmp2['mem_usage'] = mem_tmp2['mem_usage'].str.replace('GiB', '')
     mem_usag = mem_tmp2['mem_usage'].str.replace('MiB', '')
 
     mem_usag = pd.to_numeric(mem_usag, downcast='float')
 
-    usage_mean = mem_usag.mean()
+    usage_mean = mem_usag.max()
     cpu_p_mean = cpu_perc.mean()
 
     # once the file is open, we create the graph
     import matplotlib.pyplot as plt
-
-    graph, ax1 = plt.subplots()
+    plt.rcParams['axes.facecolor'] = (0.5,0.5,0.5,0.5)
+    plt.rcParams['figure.facecolor'] = (1,1,1,0.8)
+    
+    graph, ax1 = plt.subplots(2)
 
     color = 'tab:orange'
-    ax1.set_xlabel('number of measures')
+    ax1[0].set_xlabel('number of measures')
 
     if not loose_scales:
         plt.ylim([0, 600])
 
-    ax1.set_ylabel('memory usage (MiB)', color=color)
-    ax1.plot(x, mem_usag, color=color, label=f'mean memory usage: {round(usage_mean, 4)}')
-    plt.legend(loc='upper left')
-    ax1.tick_params(axis='y', labelcolor=color)
+    ax1[0].set_ylabel('memory usage (MiB)', color=color)
+    ax1[0].plot(x, mem_usag, color=color, label=f'Max memory usage: {round(usage_mean, 4)}')
+    plt.legend(loc='upper right')
+    ax1[0].tick_params(axis='y', labelcolor=color)
 
     color = 'tab:blue'
-    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+    ax2 = ax1[0].twinx()  # instantiate a second axes that shares the same x-axis
     ax2.set_ylabel('cpu percentage (%)', color=color)  # we already handled the x-label with ax1
     if not loose_scales:
         plt.ylim([0, 100])
             
     ax2.plot(x, cpu_perc, color=color, label=f'mean processor usage: {round(cpu_p_mean, 4)}')
-    plt.legend(loc='center left')
+    plt.legend(loc='upper left')
     ax2.tick_params(axis='y', labelcolor=color)
+
+    graph.tight_layout()  # otherwise the right y-label is slightly clipped
+
+    color = 'tab:green'
+
+    ax1[1].set_ylabel('Net In (kB -> MB)', color=color)  # we already handled the x-label with ax1
+    if not loose_scales:
+        plt.ylim([0, 100])
+    ax1[1].plot(x, net_in, color=color)
+    ax1[1].tick_params(axis='y')
+
+    graph.tight_layout()  # otherwise the right y-label is slightly clipped
+
+    color = 'tab:red'
+    ax4 = ax1[1].twinx()  # instantiate a second axes that shares the same x-axis
+    ax4.set_ylabel('Net Out (kB -> MB)', color=color)  # we already handled the x-label with ax1
+    if not loose_scales:
+        plt.ylim([0, 100])
+    ax4.plot(x, net_out, color=color)
+    ax4.tick_params(axis='y')
+            
 
     graph.tight_layout()  # otherwise the right y-label is slightly clipped
 
@@ -69,4 +111,4 @@ def create_stats_graph(exp_num= '', file_to_open= '', loose_scales= True, save_i
     print_centralized(' End ')
 
 if __name__ == '__main__':
-    print(create_stats_graph(exp_num= 636668609, file_to_open= 'docker_stats_4cbf7ab2b0a3.txt', save_image= 'docker_stats_4cbf7ab2b0a3.txt.svg'))
+    print(create_stats_graph(exp_num= 636668609, file_to_open= 'docker_stats_88f30141698f.txt', save_image= 'docker_stats_88f30141698f.txt.svg', home_dir=''))
