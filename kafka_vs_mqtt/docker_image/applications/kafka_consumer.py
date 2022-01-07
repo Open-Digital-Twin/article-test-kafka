87d64d133af5f845ecaeb7ec591d1ce10b8445b4
Andreo Barros <andreo.barros@2ndmarket.com.br>
2022-01-07 00:29:06 -0300

    New consumer version

--- kafka_vs_mqtt/docker_image/applications/kafka_consumer.py
+++ kafka_vs_mqtt/docker_image/applications/kafka_consumer.py
@@ -12,6 +12,7 @@ parser.add_argument("-t", "--topic", help="kafka topic to get the messages", nar
 parser.add_argument("-s", "--server", help="kafka container to connect", nargs='?', const='kafka_1', type=str, default='kafka_1')
 parser.add_argument("-p", "--server_port", help="Port where the kafka container listens", nargs='?', const='9091', type=str, default='9091')
 parser.add_argument("-n", "--n_messages", help="Sends N messages", nargs='?', const=1000, type=int, default=1000)
+parser.add_argument("-o", "--output_every", help="Outputs to file every X messages received, and at the end", nargs='?', const=100, type=int, default=100)
 
 args = parser.parse_args()
 
@@ -33,24 +34,26 @@ with open('output_consumer', 'w', buffering=1) as redf:
     redf.write('topic, kafka_timestamp, message_value, message_producer_time, message_consumer_time, consumer_produtor_latency, time_passed_since_kafka_timestamp_1, size\n')
     print("Ctrl+c to Stop")
     # Call the producer.send method with a producer-record
-    i = 0
-    for message in consumer:
+    counter = count()
+    for (index, message) in enumerate(consumer, start=1):
         time = datetime.timestamp(datetime.now())
         message_producer_time = message.value['producer_time']
         consumer_produtor_latency = time - message_producer_time
         
         message_value = message.value['value']
-        if i == 0:
+        if index == 0:
             first_message_timestamp = message.timestamp
 
         time_passage = (message.timestamp - first_message_timestamp)/1000
         contents = f'{message.topic}, {message.timestamp/1000}  , {message_value}          , {message_producer_time}    , {time}    , {consumer_produtor_latency}      ,      {time_passage}                           '
-        redf.write(f'{contents}, {str(objsize.get_deep_size(message))} \n')
-        print(f'message_number: {i}')
-        i += 1
-        if i == (number_of_messages):
+        
+        if index % args.output_every == 0:
+            redf.write(f'{contents}, {str(objsize.get_deep_size(message))} \n')
+            print(f'message_number: {index}')
+        
+        if index == (number_of_messages):
+            redf.write(f'{contents}, {str(objsize.get_deep_size(message))} \n')
             redf.close()
             exit()
             break
     exit()
-exit()
