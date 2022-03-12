e7b0d70177fc2fc7168e893eae6028fab60256cc
Andreo Barros <andreo.barros@2ndmarket.com.br>
2022-03-12 18:53:47 -0300

    Typo on the variable to save csv stats sum

--- kafka_vs_mqtt/python/kafka_project/graphics/sync.py
+++ kafka_vs_mqtt/python/kafka_project/graphics/sync.py
@@ -127,7 +127,7 @@ def sum_docker_stats(machine, file_list, exp_num, home_dir, exp_type):
 
     print(f'All files in {machine}: {file_list}')
 
-    consumer_files = [] # PORQUE CARALHOS ESSA LISTA TEM PRODUCERS
+    consumer_files = []
     for file_ in file_list:
         if ('consumer' in file_) and (machine in file_):
             consumer_files.append(file_)
@@ -135,7 +135,7 @@ def sum_docker_stats(machine, file_list, exp_num, home_dir, exp_type):
     print(f'Consumer files for machine {machine}: {consumer_files}')
 
     consumer_df = pd.read_csv(f'{file_path}csv/{consumer_files[0]}', header = 0)
-    for file_ in consumer_files:
+    for file_ in consumer_files[1:]:
         file_df = pd.read_csv(f'{file_path}csv/{file_}', header = 0)
         consumer_df = consumer_df.add(file_df, fill_value=0)
 
@@ -149,11 +149,11 @@ def sum_docker_stats(machine, file_list, exp_num, home_dir, exp_type):
     print(f'Producer files for machine {machine}: {producer_files}')
 
     producer_df = pd.read_csv(f'{file_path}csv/{producer_files[0]}', header = 0)
-    for file_ in producer_files:
+    for file_ in producer_files[1:]:
         file_df = pd.read_csv(f'{file_path}csv/{file_}', header = 0)
         producer_df = producer_df.add(file_df, fill_value=0)
 
-    consumer_df.to_csv(f'{file_path}csv/docker_producer_stats_sum_{machine}', index=False)
+    producer_df.to_csv(f'{file_path}csv/docker_producer_stats_sum_{machine}', index=False)
 
     total_df = producer_df.add(consumer_df, fill_value=0)
     total_df.to_csv(f'{file_path}csv/docker_total_stats_sum_{machine}', index=False)
